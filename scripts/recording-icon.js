6c2cc1cbf9881cb062ad3b623a2bc2b55872e67f
Konrad Dzwinel <kdzwinel@gmail.com>
2015-06-28 17:39:23 +0200

    Recording animation

--- scripts/recording-icon.js
+++ scripts/recording-icon.js
@@ -4,21 +4,38 @@ const RECORDING = 2;
 class RecordingIcon {
   construct() {
     this._status = IDLE;
+    this._animationInterval = null;
   }
 
   show() {
     this._status = RECORDING;
 
-    chrome.browserAction.setBadgeText({
-      text: '·'
-    });
+    let i = 0;
+    let animation = ' ·';
+
+    function updateFrame() {
+      var frame = animation[i % animation.length];
+      i++;
+
+      chrome.browserAction.setBadgeText({
+        text: frame
+      });
+    }
+
+    updateFrame();
+
+    this._animationInterval = setInterval(updateFrame, 150);
   }
 
   hide() {
     this._status = IDLE;
 
+    if (this._animationInterval) {
+      clearInterval(this._animationInterval);
+    }
+
     chrome.browserAction.setBadgeText({
-      text:''
+      text: ''
     });
   }
 }
